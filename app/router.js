553ea7f03e4849dd7cc43b0d7b753a4a76122e2f
F00cked <blacktha@gmail.com>
2018-08-09 13:28:14 +0800

    修改 CSS 样式

--- app/router.js
+++ app/router.js
@@ -5,8 +5,9 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
-  router.get('/greeting', controller.demo.greet);
+  router.get('/', controller.demo.greet);
+  // router.get('/', controller.home.index);
+  // router.get('/greeting', controller.demo.greet);
   router.get('/posts', controller.demo.posts);
   router.get('/posts/:id', controller.demo.getPost);
 };
